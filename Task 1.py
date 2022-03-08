3057e52d19eef395e2cbd29c3e6e2a448f0d18e4
shirazbabish <101042357+shirazbabish@users.noreply.github.com>
2022-03-08 19:23:28 +0200

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -13,7 +13,7 @@ def my_func(x1, x2, x3):
     
     if (x1+x2+x3)==0:
         return "Not a number – denominator equals zero"
-    if (x1 != float(x1) or x2 != float(x2) or x3 != float(x3)):
+    if (isinstance(x1,float) or isinstance(x2,float) or isinstance(x3,float)  == False):
         return "Error: parameters should be float"
          
     return func
